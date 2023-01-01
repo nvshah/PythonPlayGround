1fc8d03d1bb88c2d99f30483a8b19e0177235729
Nipun Shah <nipunshah6776@gmail.com>
2023-01-02 00:38:18 +0530

    new utils

---
+++ Data Structure And Algorithms/Maths/random_concept.py
@@ -0,0 +1,5 @@
+import random
+
+# This will not pick in sorted manner
+s = random.sample(range(1, 20), 5)
+print(s)
--- Python Short Concept Snippets/dict_utils.py
+++ Python Short Concept Snippets/dict_utils.py
@@ -1,4 +1,16 @@
 # dict_utils
 
+from collections import defaultdict
+from itertools import count
+
 def dict_from_keys_values(keys, values):
     return dict(zip(keys, values))
+
+
+def default_dict_incrementer():
+    '''default dict that assigns default value in an increment fashion
+       concepts includes :- Closure & Free variables
+    '''
+    cntr = it.count()
+    incrementer = ct.defaultdict(cntr.__next__)
+    return incrementer
